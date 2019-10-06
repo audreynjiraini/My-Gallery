d9ce469d9c363914f1a03fcd889e2f817422aff2
Audrey Wanjiku <audreynjiraini@gmail.com>
2019-10-06 23:53:02 +0300

    create setUp and isinstance tests for Location class

--- photos/tests.py
+++ photos/tests.py
@@ -1,3 +1,28 @@
 from django.test import TestCase
+from .models import Location, Category, Image
+
 
 # Create your tests here.
+
+class LocationTestClass(TestCase):
+    '''
+    Tests for Location class.
+    '''
+    
+    def setUp(self):
+        '''
+        Runs before each test.
+        '''
+        
+        self.location = Location(id = 50, name = 'Kenya')
+        
+        
+    def test_instance(self):
+        '''
+        Checks if object is an instance of the Location class.
+        '''
+        
+        self.assertTrue(isinstance(self.location, Location))
+        
+        
+    
