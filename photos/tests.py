40cd9f5913e404f52904096aa45e349473df6403
Audrey Wanjiku <audreynjiraini@gmail.com>
2019-10-06 23:59:17 +0300

    create tests for the save, update and delete methods for the Location class

--- photos/tests.py
+++ photos/tests.py
@@ -25,4 +25,31 @@ class LocationTestClass(TestCase):
         self.assertTrue(isinstance(self.location, Location))
         
         
-    
+    def test_save_method(self):
+        '''
+        Test whetherthe object is saved in the database.
+        '''
+        
+        self.location.save_location()
+        locations = Location.objects.all()
+        self.assertTrue(len(locations) > 0)
+        
+        
+    def test_update_method(self):
+        '''
+        Test if the object can be updated.
+        '''
+        self.location.save_location()
+        self.location = Location.objects.filter(name = 'Kenya').update(name = 'Burundi')
+        self.updated_location = Location.objects.get(name = 'Burundi')
+        self.assertEqual(self.updated_location.name,"Burundi")
+
+
+    def test_delete_method(self):
+        '''
+        Test if the object can be deleted from the database.
+        '''
+        self.location.save_location()
+        self.location = Location.objects.get(id = 50)
+        self.location.delete_location()
+        self.assertTrue(len(Location.objects.all()) == 0)
